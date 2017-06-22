52220dd19c1dbf59de6dd7622dbe50ed773d6347
Casper Löfgren <casper.lofgren@hotmail.com>
2017-06-22 21:11:28 +0300

    Add author information to ChampionAPI.cs

--- champion-desktop/ChampionAPI.cs
+++ champion-desktop/ChampionAPI.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Champion.gg API wrapper
+ * Initial author: Casper Löfgren
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
